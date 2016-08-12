d9d47d6a12b6ba8a96a11245ab2cfbee57e7fabc
josh <josh@joshs-Air.home>
2016-08-12 16:11:23 -0600

    added a gitignore for the private, and styling for active nav with jquery

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 	console.log("the Document is ready");
 	var bars = $("#goToLog");
+	var plus = $("#goToAdd");
 	bars.click(function(){
 		console.log("clicked the bars");
+		plus.removeClass("active");
+		bars.addClass("active");
+	});
+	plus.click(function(){
+		console.log("plus clicked");
+		bars.removeClass("active");
+		plus.addClass("active");
 	});
 });
