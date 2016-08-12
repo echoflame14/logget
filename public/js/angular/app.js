830d9299ef31b9e8f300f251e844d310bae735d8
josh <josh@joshs-Air.home>
2016-08-12 14:56:38 -0600

    Added styling for the top nav

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -1,1 +1,15 @@
-angular.module("app", []);
+angular
+.module("app", ["ui.router"])
+.config(["$urlRouterProvider", "$stateProvider", function($urlRouterProvider, $stateProvider){
+	console.log($urlRouterProvider);
+	console.log($stateProvider);
+
+	$urlRouterProvider.otherwise("/"); // bounces the user back to / when the http request does not match a state
+	$stateProvider
+		.state("home", {
+			url: "/",
+			templateUrl: "../html/views/home.html"
+		})
+		.state("");
+
+}]);
