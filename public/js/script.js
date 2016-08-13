1e5dc2bc569726b7ffa5bd51d53ff5728b90e2af
josh <josh@joshs-MacBook-Air.local>
2016-08-13 14:01:45 -0600

    fixed accordian menu.

--- public/js/script.js
+++ public/js/script.js
@@ -12,4 +12,44 @@ $(document).ready(function(){
 		bars.removeClass("active");
 		plus.addClass("active");
 	});
+	//end viewSwitch
+
+	//start accordian
+	var common = $("#common");
+	var otherCat = $("#otherCat");
+	var otherCat1 = $("#otherCat1");
+
+	otherCat.click(function(){
+		if(otherCat.hasClass("collapsedMenu")){
+			console.log("otherCat has been clickd");
+			otherCat.removeClass("collapsedMenu");
+			common.addClass("collapsedMenu");
+			otherCat1.addClass("collapsedMenu");
+		}
+		else{
+			otherCat.addClass("collapsedMenu");
+		}
+	});
+	common.click(function(){
+		if(common.hasClass("collapsedMenu")){
+			console.log("common has been clickd");
+			common.removeClass("collapsedMenu");
+			otherCat.addClass("collapsedMenu");
+			otherCat1.addClass("collapsedMenu");
+		}
+		else{
+			common.addClass("collapsedMenu");
+		}
+	});
+	otherCat1.click(function(){
+		if(otherCat1.hasClass("collapsedMenu")){
+			console.log("otherCat has been clickd");
+			otherCat1.removeClass("collapsedMenu");
+			common.addClass("collapsedMenu");
+			otherCat.addClass("collapsedMenu");
+		}
+		else{
+			otherCat1.addClass("collapsedMenu");
+		}
+	});
 });
