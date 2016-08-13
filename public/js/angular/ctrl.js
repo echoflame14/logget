a69ea0f9cf51a4791069ede5535704fff28dbecb
josh <josh@joshs-MacBook-Air.local>
2016-08-13 13:18:08 -0600

    fa for the log col's

--- public/js/angular/ctrl.js
+++ public/js/angular/ctrl.js
@@ -1,4 +1,8 @@
 angular.module("app").controller("ctrl",function($scope,srvc){
 	$scope.test = "Hey I'm working!!";
 	$scope.peopleList = srvc.people;
+	$scope.placeList = srvc.places;
+	$scope.timeList = srvc.times;
+	$scope.thingList = srvc.things;
+	$scope.actionList = srvc.actions;
 });
--- public/js/angular/srvc.js
+++ public/js/angular/srvc.js
@@ -1,13 +1,33 @@
 angular.module("app").service("srvc", function(){
-	this.people = {
-		p1: {
-			name:"josh"
-		},
-		p2: {
-			name:"tori"
-		},
-		p3: {
-			name:"dad"
-		}
-	};
+	this.people = [
+		{name:"josh"},
+		{name:"tori"},
+		{name:"dade"}
+	];
+	this.places = [
+		{name:"provo"},
+		{name:"kaysville"},
+		{name:"devmountain"}
+	];
+	this.times = [
+		{name:"now"},
+		{name:"Morning"},
+		{name:"Noon"},
+		{name:"Evening"},
+		{name:"Night"}
+	];
+	this.things = [
+		{name:"Streetfighter 5"},
+		{name:"The BFG"},
+		{name:"Doom"},
+		{name:"Suicide Squid"}
+	];
+	this.actions = [
+		{name:"Eating"},
+		{name:"Watching"},
+		{name:"Sleeping"},
+		{name:"Working"},
+		{name:"Drawing"},
+		{name:"Church"}
+	];
 });
